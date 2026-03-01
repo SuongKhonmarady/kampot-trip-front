28a460ee1ece37622bf7f1fca2af86be0627eb4c
marady-work <nevermind99887@gmail.com>
2026-03-01 20:49:03 +0700

    Update API base URL to new endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.tosderleng.tech/api',
+  baseURL: 'https://kampot-trip-backend.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
